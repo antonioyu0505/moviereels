c2974ab51d75830ea9f38810d14552227c5e9a41
Antonio Yu <antonio.yu@mail.polimi.it>
2021-05-26 02:40:57 -0500

    Modified the search controller to adapt the schema

--- mrbackend/src/controller/searchController.py
+++ mrbackend/src/controller/searchController.py
@@ -1,15 +1,16 @@
 from mrbackend import app
 from mrbackend.src.service.search import Search
+from mrbackend.src.schema.movieSchema import MovieSchema
 from flask import request, jsonify
+
+moviesSchema = MovieSchema(many=True)
 
 @app.route('/search/tv', methods=['GET'])
 def searchTV():
-  results = []
-  for result in Search.tv(request.args.get('query'))[1]: results.append(result.__dict__)
-  return jsonify(results)
+  q = Search.tv(request.args.get('query'))[1]
+  return moviesSchema.jsonify(moviesSchema.dump(q))
 
 @app.route('/search/movie', methods=['GET'])
 def searchMovie():
-  results = []
-  for result in Search.movie(request.args.get('query'))[1]: results.append(result.__dict__)
-  return jsonify(results)
+  q = Search.movie(request.args.get('query'))[1]
+  return moviesSchema.jsonify(moviesSchema.dump(q))
