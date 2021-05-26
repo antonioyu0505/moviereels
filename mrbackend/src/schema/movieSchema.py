7552275fed8f0d4e2706f0a92415caeab0a4e2e6
Antonio Yu <antonio.yu@mail.polimi.it>
2021-05-26 02:40:47 -0500

    Added a movie schema to serialize/deserializa data

---
+++ mrbackend/src/schema/movieSchema.py
@@ -0,0 +1,13 @@
+from mrbackend.src.extensions import ma
+from marshmallow import post_dump
+from unicodedata import normalize
+
+class MovieSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'name', 'overview', 'posterPath')
+
+  @post_dump
+  def normalizeCharacters(self, data, **kwargs):
+    for field in data:
+      if isinstance(data[field], str): data[field] = normalize('NFKD', data[field])
+    return data
