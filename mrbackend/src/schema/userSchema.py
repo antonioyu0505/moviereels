8cc76610cf5577b3e966f8f92670907ad7426482
Antonio Yu <antonio.yu@mail.polimi.it>
2021-05-26 02:05:51 -0500

    Moved the schema variable to the controller

--- mrbackend/src/schema/userSchema.py
+++ mrbackend/src/schema/userSchema.py
@@ -3,6 +3,3 @@ from mrbackend.src.extensions import ma
 class UserSchema(ma.Schema):
   class Meta:
     fields = ('id', 'username', 'countryCode', 'lastLogin', 'password')
-
-userSchema = UserSchema()
-usersSchema = UserSchema(many = True)
