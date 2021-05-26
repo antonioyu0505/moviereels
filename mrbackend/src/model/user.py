d4bb410affef1031b954ee0536f32437a462968d
Antonio Yu <antonio.yu@mail.polimi.it>
2021-05-26 02:05:38 -0500

    Removed database attributes since it is reflected

--- mrbackend/src/model/user.py
+++ mrbackend/src/model/user.py
@@ -1,20 +1,8 @@
-from mrbackend.src.extensions import db
-from datetime import datetime
+class User():
 
-class User(db.Model):
-  id = db.Column(db.Integer, primarykey = True)
-  username = db.Column(db.String(50), unique = True)
-  password = db.Column(db.Text)
-  countryCode = db.Column(db.String(2))
-  lastLogin = db.Column(db.DateTime)
-
-  def __init__(self, username, password, countryCode):
+  def __init__(self, id, username, password, countryCode, lastLogin):
+    self.id = id
     self.username = username
     self.password = password
     self.countryCode = countryCode
-    self.lastLogin = datetime.now()
-
-  def getUsername(self): print(self.username)
-  def getPassword(self): print(self.password)
-  def getCountryCode(self): print(self.countryCode)
-  def getlastLogin(self): print(self.lastLogin)
+    self.lastLogin = lastLogin
