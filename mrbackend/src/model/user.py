94dc29faba972f6db0770a224098c9ebe0b45038
Antonio Yu <antonio.yu@mail.polimi.it>
2021-05-25 20:28:34 -0500

    Added getter methods to User model

--- mrbackend/src/model/user.py
+++ mrbackend/src/model/user.py
@@ -2,14 +2,19 @@ from mrbackend.src.extensions import db
 from datetime import datetime
 
 class User(db.Model):
-  id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(50), unique = True)
-  password = db.Column(db.Text)
-  countryCode = db.Column(db.String(2))
-  lastLogin = db.Column(db.DateTime)
+  _id = db.Column(db.Integer, primary_key = True)
+  _username = db.Column(db.String(50), unique = True)
+  _password = db.Column(db.Text)
+  _countryCode = db.Column(db.String(2))
+  _lastLogin = db.Column(db.DateTime)
 
   def __init__(self, username, password, countryCode):
-    self.username = username;
-    self.password = password;
-    self.countryCode = countryCode;
-    self.lastLogin = datetime.now();
+    self._username = username
+    self._password = password
+    self._countryCode = countryCode
+    self._lastLogin = datetime.now()
+
+  def getUsername(self): print(self._username)
+  def getPassword(self): print(self._password)
+  def getCountryCode(self): print(self._countryCode)
+  def getlastLogin(self): print(self._lastLogin)
