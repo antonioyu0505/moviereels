5b1436d941ca289a8622b08baa82588ee9f47379
Antonio Yu <antonio.yu@mail.polimi.it>
2021-05-27 01:05:17 -0500

    Instanciated SearchService

--- mrbackend/src/controller/searchController.py
+++ mrbackend/src/controller/searchController.py
@@ -1,16 +1,17 @@
 from mrbackend import app
-from mrbackend.src.service.search import Search
+from mrbackend.src.service.searchService import SearchService
 from mrbackend.src.schema.movieSchema import MovieSchema
-from flask import request, jsonify
+from flask import request
 
 moviesSchema = MovieSchema(many=True)
+searchService = SearchService()
 
 @app.route('/search/tv', methods=['GET'])
 def searchTV():
-  q = Search.tv(request.args.get('query'))[1]
+  q = searchService.tv(request.args.get('query'))[1]
   return moviesSchema.jsonify(moviesSchema.dump(q))
 
 @app.route('/search/movie', methods=['GET'])
 def searchMovie():
-  q = Search.movie(request.args.get('query'))[1]
+  q = searchService.movie(request.args.get('query'))[1]
   return moviesSchema.jsonify(moviesSchema.dump(q))
